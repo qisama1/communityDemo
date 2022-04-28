27189b822f2bf339baca412ac651d52f800f6c18
azxx <996800442@qq.com>
2022-04-28 09:08:20 +0800

    关注列表和粉丝列表

--- src/main/java/com/newcoder/communitydemo/controller/FollowController.java
+++ src/main/java/com/newcoder/communitydemo/controller/FollowController.java
@@ -1,22 +1,34 @@
 package com.newcoder.communitydemo.controller;
 
+import com.newcoder.communitydemo.entity.Page;
 import com.newcoder.communitydemo.entity.User;
 import com.newcoder.communitydemo.service.FollowService;
+import com.newcoder.communitydemo.service.UserService;
+import com.newcoder.communitydemo.util.CommunityConstant;
 import com.newcoder.communitydemo.util.CommunityUtil;
 import com.newcoder.communitydemo.util.HostHolder;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import java.util.List;
+import java.util.Map;
+
 @Controller
-public class FollowController {
+public class FollowController implements CommunityConstant{
 
     @Autowired
     private FollowService followService;
 
     @Autowired
     private HostHolder hostHolder;
+
+    @Autowired
+    private UserService userService;
 
     @PostMapping("/follow")
     @ResponseBody
@@ -38,4 +50,59 @@ public class FollowController {
         return CommunityUtil.getJSONString(0, "已取消关注");
     }
 
+    @GetMapping("/followees/{userId}")
+    public String getFollowees(@PathVariable("userId") int userId, Page page, Model model) {
+        User user = userService.findUserById(userId);
+        if (user == null) {
+            throw new RuntimeException("用户不存在！");
+        }
+        model.addAttribute("user", user);
+
+        page.setLimit(5);
+        page.setPath("/followees/" + userId);
+        page.setRows((int) followService.findFolloweeCount(userId, ENTITY_TYPE_USER));
+
+        List<Map<String, Object>> userList = followService.findFollowees(userId, page.getOffset(), page.getLimit());
+        if (userList != null) {
+            for (Map<String, Object> map : userList) {
+                User u = (User) map.get("user");
+                map.put("hasFollowed", hasFollowed(u.getId()));
+            }
+        }
+        model.addAttribute("users", userList);
+
+        return "/site/followee";
+    }
+
+    private boolean hasFollowed(int userId) {
+        if (hostHolder.getUser() == null) {
+            return false;
+        }
+
+        return followService.hasFollowed(hostHolder.getUser().getId(), ENTITY_TYPE_USER, userId);
+    }
+
+    @GetMapping("/followers/{userId}")
+    public String getFollowers(@PathVariable("userId") int userId, Page page, Model model) {
+        User user = userService.findUserById(userId);
+        if (user == null) {
+            throw new RuntimeException("用户不存在！");
+        }
+        model.addAttribute("user", user);
+
+        page.setLimit(5);
+        page.setPath("/followers/" + userId);
+        page.setRows((int) followService.findFollowerCount(ENTITY_TYPE_USER, userId));
+
+        List<Map<String, Object>> userList = followService.findFollowers(userId, page.getOffset(), page.getLimit());
+        if (userList != null) {
+            for (Map<String, Object> map : userList) {
+                User u = (User) map.get("user");
+                map.put("hasFollowed", hasFollowed(u.getId()));
+            }
+        }
+        model.addAttribute("users", userList);
+
+        return "/site/follower";
+    }
 }
